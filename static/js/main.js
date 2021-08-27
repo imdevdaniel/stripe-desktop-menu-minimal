a93a48a3a8d6b489f2939aaba7785cc0fd35eba2
Daniel Pacheco <imdevdaniel@gmail.com>
2021-08-27 08:59:21 -0600

    Adds gradient background, changes colors, fixes transitions

--- static/js/main.js
+++ static/js/main.js
@@ -10,6 +10,9 @@ window.onload = () => {
     const menuOptions = document.querySelectorAll('[data-menu-opt]');
     const menuSections = document.querySelectorAll('[data-section]');
     const menuContainer = document.querySelector('.desktop-menu');
+    const headerContainer = document.querySelector('.header');
+
+    menuContainer.classList.add('desktop-menu--rotate');
 
     for (section of menuSections) {
         const sectionName = section.dataset.section;
@@ -23,7 +26,6 @@ window.onload = () => {
         element.addEventListener('mouseenter', (e) => {
             const targetSection = e.target.dataset.menuOpt;
             const targetElement = document.querySelector(`.section-${targetSection}`);
-            console.log('Target section to enable', targetElement);
             for (section of menuSections) {
                 section.classList.remove('desktop-menu__section--visible');
             }
@@ -31,11 +33,31 @@ window.onload = () => {
                 'style',
                 `height: ${mainMenuSizes[targetSection].height}px; width: ${mainMenuSizes[targetSection].width}px;`
             );
+            menuContainer.classList.add('desktop-menu--visible');
             targetElement.classList.add('desktop-menu__section--visible');
+            menuContainer.addEventListener('transitionend', addResizeAnim);
         });
     }
 
-    // console.log('New values for mainmenusizes:', mainMenuSizes);
+    const addResizeAnim = (e) => {
+        if (!menuContainer.classList.contains('desktop-menu--anim-resize')) {
+            menuContainer.classList.add('desktop-menu--anim-resize');
+            for (section of menuSections) {
+                section.setAttribute('style', 'transition-duration: 0.25s');
+            }
+        }
+        menuContainer.removeEventListener('transitionend', addResizeAnim);
+    };
+
+    headerContainer.addEventListener('mouseleave', () => {
+        menuContainer.classList.remove('desktop-menu--visible');
+        menuContainer.classList.remove('desktop-menu--anim-resize');
+        for (section of menuSections) {
+            section.removeAttribute('style');
+        }
+    });
+
+    console.log('New values for mainmenusizes:', mainMenuSizes);
 
     // menuContainer.setAttribute('style', `height: ${mainMenuSizes.prods.height}px; width: ${mainMenuSizes.prods.width}px;`);
 
